2c47d75a53092d5b015486d092c6728cd97f4f1a
Flavio Poletti <flavio@polettix.it>
2018-08-30 13:35:00 +0200

    Advance in making gpg work...

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 for profile in "$(dirname "$0")"/.profile.d/*.sh ; do
    . "$profile"
@@ -9,7 +10,34 @@ PATH="/app:$PATH"
 export GNUPGHOME=/tmp/gnupg
 mkdir "$GNUPGHOME"
 chmod og-rwx "$GNUPGHOME"
-cp /mnt/.gnupg/pubring.gpg /mnt/.gnupg/secring.gpg /mnt/.gnupg/trustdb.gpg \
-   "$GNUPGHOME"
+
+if [ -f '/mnt/.gnupg' ] ; then
+   tar xC "$GNUPGHOME" -f /mnt/.gnupg
+elif [ -d '/mnt/.gnupg' ] ; then
+   for f in pubring.gpg secring.gpg trustdb.gpg .gpg-v21-migrated ; do
+      path="/mnt/.gnupg/$f"
+      [ -e "$path" ] || continue
+      cp "$path" "$GNUPGHOME"
+   done
+   if [ -d "/mnt/.gnupg/private-keys-v1.d" ] ; then
+      mkdir -p "$GNUPGHOME/private-keys-v1.d"
+      cp /mnt/.gnupg/private-keys-v1.d/* "$GNUPGHOME/private-keys-v1.d"
+   fi
+elif [ -e '/mnt/.gnupg-export' ] ; then
+   source='/mnt/.gnupg-export'
+   if [ -f "$source" ] ; then
+      file="$source"
+      source="$GNUPGHOME/exports"
+      mkdir -p "$source"
+      tar xC "$source" -f "$file"
+   fi
+   for f in secret public ; do
+      [ -e "$source/$f" ] && gpg --import < "$source/$f"
+   done
+   [ -e "$source/ownertrust" ] \
+      && gpg --import-ownertrust < "$source/ownertrust"
+else
+   printf >&2 '%s\n' 'cannot set gnupg up properly'
+fi
 
 exec /bin/sh
